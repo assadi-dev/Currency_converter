922c0931d5d6209e8bd81867cc087a35882428c6
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-06 22:02:18 +0100

    Retour json non connecté daans le middleware

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\CurrencyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
+Route::middleware('auth:sanctum')->get('/currencies', [CurrencyController::class, 'index']);
