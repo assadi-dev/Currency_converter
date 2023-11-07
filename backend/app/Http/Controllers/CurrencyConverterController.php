249e35673548e501f2ed596eaa4d05918676161a
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-07 11:44:19 +0100

    update des paires

---
+++ backend/app/Http/Controllers/CurrencyConverterController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CurrencyConversionPair;
+use Illuminate\Http\Request;
+
+class CurrencyConverterController extends Controller
+{
+    private $currency_pair_converters;
+
+
+    public function __construct(CurrencyConversionPair $currency_pair_converters)
+    {
+        $this->currency_pair_converters = $currency_pair_converters;
+    }
+
+    /**
+     * @param  string  $from_currency
+     * @param  string  $to_currency
+     * @param  int  $amount
+     * @return \Illuminate\Http\Response
+     */
+    public function convert($from_currency, $to_currency, $amount)
+    {
+
+
+
+        try {
+
+            $exchange_rate = $this->extract_currency_rate($from_currency, $to_currency);
+
+            $converted = $amount * $exchange_rate;
+
+
+
+            return response()->json([
+                'message' => 'Conversion effectué',
+                'from' => $from_currency,
+                'to' => $to_currency,
+                'result' => $converted,
+                'pairCurrency' => $exchange_rate
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'server error',
+                'errors' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function extract_currency_rate($from_currency, $to_currency)
+    {
+
+        $exchange_rate = $this->currency_pair_converters->findByExchangeRate($from_currency, $to_currency)->exchange_rate;
+
+        return $exchange_rate;
+
+    }
+
+
+
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\CurrencyController;
 use App\Http\Controllers\CurrencyConversionPairsController;
+use App\Http\Controllers\CurrencyConverterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,3 +32,4 @@ Route::put('/currency/{id}', [CurrencyController::class, 'update']);
 Route::delete('/currency/{id}', [CurrencyController::class, 'destroy']);
 Route::get('/currency_convert_pairs', [CurrencyConversionPairsController::class, 'index']);
 Route::post('/currency_convert_pairs', [CurrencyConversionPairsController::class, 'store']);
+Route::get('/currency_converter/{from_currency}/{to_currency}/{amount}', [CurrencyConverterController::class,'convert']);
