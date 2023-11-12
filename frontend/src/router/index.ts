829d706d531770ce80c92965fc35cbcf01e697e2
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-12 22:26:18 +0100

    Page 404

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,60 +1,61 @@
-
-
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeViewVue from '../views/HomeView/HomeView.vue'
-import HomeAdminViewVue from '../views/Admin/HomeAdminView.vue'
-import LoginVue from '../views/Authentication/Login.vue'
-import AdminLayoutVue from '../views/Admin/AdminLayout.vue'
-import DashboardViewVue from '../views/Admin/DashboardView/DashboardView.vue'
-import CurrencyViewVue from '../views/Admin/CurrencyView/CurrencyView.vue'
-import PairCurrencyViewVue from '../views/Admin/PairCurrencyView/PairCurrencyView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeViewVue from "../views/HomeView/HomeView.vue";
+import HomeAdminViewVue from "../views/Admin/HomeAdminView.vue";
+import LoginVue from "../views/Authentication/Login.vue";
+import AdminLayoutVue from "../views/Admin/AdminLayout.vue";
+import DashboardViewVue from "../views/Admin/DashboardView/DashboardView.vue";
+import CurrencyViewVue from "../views/Admin/CurrencyView/CurrencyView.vue";
+import PairCurrencyViewVue from "../views/Admin/PairCurrencyView/PairCurrencyView.vue";
+import NoFoundView from "../views/ErrorView/NoFoundView.vue"
+import AdminNoFound from "../views/ErrorView/AdminNoFound.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeViewVue
+      path: "/",
+      name: "home",
+      component: HomeViewVue,
     },
     {
-      path: '/admin/login',
-      name: 'admin-login',
-      component: LoginVue
-},
+      path: "/admin/login",
+      name: "admin-login",
+      component: LoginVue,
+    },
     {
-      path: '/admin',
-   
-      component:AdminLayoutVue,
-      children: [
+      path: "/admin",
 
+      component: AdminLayoutVue,
+      children: [
         {
-          path: '',
-         
+          path: "",
+
           component: HomeAdminViewVue,
           children: [
-     
             {
-              path: 'currencies',
-              name: 'admin-currencies',
-              component: CurrencyViewVue
+              path: "currencies",
+              name: "admin-currencies",
+              component: CurrencyViewVue,
             },
             {
-              path: 'pair-currencies',
-              name: 'admin-pair-currency',
-              component: PairCurrencyViewVue
+              path: "pair-currencies",
+              name: "admin-pair-currency",
+              component: PairCurrencyViewVue,
             },
-          ]
+            {
+              path: `:notFound`,
+              component:AdminNoFound
+            }
+          ],
         },
-      ]
+      ],
+    },
+    {
+      path: `/:notFound`,
+      component:NoFoundView
     }
+  ],
+  
+});
 
-
-  ]
-})
-
-
-
-
-export default router
+export default router;
---
+++ frontend/src/views/ErrorView/NoFoundView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <div class="flex flex-column align-items-center justify-content-center">
+            <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, rgba(33, 150, 243, 0.4) 10%, rgba(33, 150, 243, 0) 30%)">
+                <div class="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center" style="border-radius: 53px">
+                    <span class="text-blue-500 font-bold text-3xl">404</span>
+                    <h1 class="text-900 font-bold text-3xl lg:text-5xl mb-2">Erreur !</h1>
+                    <div class="text-600 mb-5">Desolé vous essayé d'acceder à une page qui n'existe pas.</div>
+                    <Button label="Revenire à la page d'accueil" icon="pi pi-home" class="p-button-text" @click="goHome" />
+
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router"
+import Button from "primevue/button"
+const router = useRouter()
+
+
+const goHome = () => {
+    router.push({name:"home"})
+}
+
+</script>
+
+<style scoped>
+
+</style>
